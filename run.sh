d2308efd6bd8cdb175840e0a256f8bab95d62402
Thomas Toye <thomas@toye.io>
2017-03-09 15:13:26 +0100

    Echo steps during run.sh execution

--- run.sh
+++ run.sh
@@ -5,33 +5,33 @@ UGENT_USERNAME=trtoye
 # Run script
 node main.js
 
-# Save calendar contents into variable
+echo " >> Save calendar contents into variable"
 ICS_CONTENTS=`cat calendar-${UGENT_USERNAME}.ics`
 
-# Remove existing calendar file on this branch
+echo " >> Remove existing calendar file on this branch"
 rm calendar-${UGENT_USERNAME}.ics
 
-# Stash changed files
+echo " >> Stash changed files"
 git stash
 
-# Switch to gh-pages branch
+echo " >> Switch to gh-pages branch"
 git checkout gh-pages
 
-# Update calendar
+echo " >> Update calendar"
 echo $ICS_CONTENTS > calendar-${UGENT_USERNAME}.ics
 
-# Add file
+echo " >> Add file"
 git add calendar-${UGENT_USERNAME}.ics
 
-# Commit
+echo " >> Commit"
 git commit -m "Update calendar for $UGENT_USERNAME"
 
-# Push
+echo " >> Push"
 git push orgin gh-pages
 
-# Back to master
+echo " >> Back to master"
 git checkout master
 
-# Get stash back
+echo " >> Get stash back"
 git stash apply
 
