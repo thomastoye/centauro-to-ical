6454248bece122c911859ca16e1d9b79bb411d7f
Thomas Toye <thomas@toye.io>
2017-03-09 15:31:22 +0100

    Because of the Windows-style line endings in the ICS file spec, bash corrupted the file
    Copy to temp file instead

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 UGENT_USERNAME=trtoye
 
+TMPFILE=$(mktemp /tmp/centauro-to-ics-script.XXXXXX)
+
 # Run script
 node main.js
 
-echo " >> Save calendar contents into variable"
-ICS_CONTENTS=`cat calendar-${UGENT_USERNAME}.ics`
+echo " >> Save calendar contents into temp file"
+cp calendar-${UGENT_USERNAME}.ics $TMPFILE
 
 echo " >> Remove existing calendar file on this branch"
 rm calendar-${UGENT_USERNAME}.ics
@@ -18,7 +20,7 @@ echo " >> Switch to gh-pages branch"
 git checkout gh-pages
 
 echo " >> Update calendar"
-echo $ICS_CONTENTS > calendar-${UGENT_USERNAME}.ics
+mv $TMPFILE calendar-${UGENT_USERNAME}.ics
 
 echo " >> Add file"
 git add calendar-${UGENT_USERNAME}.ics
